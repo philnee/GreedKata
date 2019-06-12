a4b35b6c907a2ca11fa8d2b007c52fa9525ec99e
philnee <pheelter@gmail.com>
2019-06-12 08:18:30 +0100

    Starting point

---
+++ Greed.Tests/Tests.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using Greed;
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(true, true);
+        }
+    }
+
+}
---
+++ Greed/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Greed
+{
+    public class Game
+    {
+        public int Score(int[] dice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
